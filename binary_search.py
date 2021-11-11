c21000b7e314a7e5ecbc0c7446948e8f3cafeb9a
DeathSpace <deathspacedesign@gmail.com>
2021-11-11 16:33:53 +0530

    first commit

---
+++ binary_search.py
@@ -0,0 +1,19 @@
+def binary_search(list, target):
+    first = 0
+    last = len(list) - 1
+
+    while(first <= last):
+        middle = (first + last) // 2
+
+        if(list[middle] == target):
+            return middle
+        elif(list[middle] < target):
+            first = middle + 1
+        else:
+            last = middle - 1
+    return -1
+
+
+list = [1, 2, 3, 4, 5, 6, 7, 8]
+
+print(binary_search(list, 8))
---
+++ recursive_binary_search.py
@@ -0,0 +1,33 @@
+def recursive_binary_search(list, target):
+    if len(list) == 0:
+        return False
+    else:
+        midpoint = (len(list)) // 2
+
+        if (list[midpoint] == target):
+            return True
+        else:
+            if (list[midpoint] < target):
+                return recursive_binary_search(list[midpoint+1:], target)
+            else:
+                return recursive_binary_search(list[:midpoint], target)
+
+
+list = [1, 2, 3, 4, 5, 6, 7, 8]
+
+
+def verify(result):
+    print('Target exists:', result)
+
+
+result = recursive_binary_search(list, 0)
+verify(result)
+
+result = recursive_binary_search(list, 8)
+verify(result)
+
+result = recursive_binary_search(list, 1)
+verify(result)
+
+result = recursive_binary_search(list, 12)
+verify(result)
